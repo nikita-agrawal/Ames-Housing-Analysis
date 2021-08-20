2eecba89cdd2852a225cf1df8c741963cdc6b6ae
Hayden Warren <haydenlw4@gmail.com>
2021-08-20 11:05:44 -0400

    fixed documentation for helper. doc string for stratified_split and added proper authorship credit.

--- Hayden/helper.py
+++ Hayden/helper.py
@@ -3,16 +3,44 @@
 """
 Created on Thu Aug 19 08:55:00 2021
 
-@author: haydenlw4
+@author: Alex Austin, Hayden Warren
 """
 
 import pandas as pd
 from sklearn.model_selection import StratifiedShuffleSplit
 
+# Stratified split authored by Alex A, generalized by Hayden W.
 def stratified_split(df,stratified_col,
                      n_splits = 1, 
                      test_size = 0.25, 
                      random_state = 42):
+    '''
+    Completes a stratified split on unique rows of a dataframe 
+    on specified column.
+    
+    Parameters
+    ----------
+    df : pandas Dataframe
+        Dataframe to be split.
+    stratified_col : string
+        Column that stratification will be done on.
+    n_splits : integer, optional
+        number of splits. The default is 1.
+    test_size : float, optional
+        proportion in df_test. The default is 0.25.
+    random_state : integer, optional
+        The default is 42.
+
+    Returns
+    -------
+    df_train : pandas Dataframe
+        Dataframe after split. 
+        Will have 1 - test_size proportion of rows.
+    df_test : pandas Dataframe
+        Dataframe after split. 
+        Will have test_size proportion of rows.
+
+    '''
     # Ensure clean index and remove duplicates
     df.reset_index(inplace=True,drop=True)
     df.drop_duplicates(inplace=True)

2befc31681614fd701a910645490c75bb4afae11
Hayden Warren <haydenlw4@gmail.com>
2021-08-30 13:58:54 -0400

    trying to have geo_ data frames to match normal train/split

--- Hayden/test_train_split_geo.py
+++ Hayden/test_train_split_geo.py
@@ -9,15 +9,24 @@ Created on Sat Aug 28 19:14:24 2021
 import pandas as pd
 import helper
 
-housing = pd.read_csv('ames_housing_latlong.csv', index_col=0)
+housing = pd.read_csv('ames_housing_latlong.csv', index_col = 0,
+                      low_memory=False)
+
+housing.drop_duplicates(inplace=True)
+housing.reset_index(drop=True, inplace=True)
+
+housing = housing[(housing['SaleCondition'] == 'Normal') | 
+                   (housing['SaleCondition'] == 'Partial')].reset_index(drop=True)
 
 train, test = helper.stratified_split(housing,'Neighborhood')
 
 train, mapping_dict = helper.geo_cords_imputing(train)
 test = helper.geo_cords_imputing(test,mapping_dict)
+
 # note there are three houses that didn't have address values and were not
 # in neighborhoods to have their geo-coordinates computed. 
 # They are all in train.
 # their PID are: [916252170, 916253320, 907230240]
+
 train.to_csv('train_geo.csv')
 test.to_csv('test_geo.csv')
